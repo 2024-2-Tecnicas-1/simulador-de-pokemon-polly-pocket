44e401f7deeaa0f25dacb820488c5b16470131b5
Ksrenn <karen170bayona@hotmail.com>
2024-11-12 01:28:35 -0500

    Entrenador prueba 1
    Se implementa el nombre al entrenador, y un switch al menu principal

--- src/main/java/simulador/entrenador/Entrenador.java
+++ src/main/java/simulador/entrenador/Entrenador.java
@@ -1,10 +1,30 @@
 package simulador.entrenador;
+
+import java.util.List;
+import java.util.Scanner;
+
 /**
  *
  * @author dfpar
- * @author 
- * @author 
- */
+ * @author
+ * @author
+// */
 public class Entrenador {
-    // TODO: Aquí va tu código
+
+    private String nombre;
+    private List<String> pokemones;
+
+    //Constructor
+    public Entrenador(String nombre, List<String> pokemones) {
+        this.nombre = nombre;
+        this.pokemones = pokemones;
+    }
+    Scanner scan = new Scanner(System.in);
+    
+    //Obtener nombre del entrenador
+    String nombreentrenador = scan.next();
+    public String getnombre() {
+        return nombre = nombreentrenador;
+    }
+
 }

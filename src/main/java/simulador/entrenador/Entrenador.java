1d310fb224ec674fe45e74789cf7da4dbc455ff7
Ksrenn <karen170bayona@hotmail.com>
2024-11-12 20:17:32 -0500

    Implementaciones a Entrenador

--- src/main/java/simulador/entrenador/Entrenador.java
+++ src/main/java/simulador/entrenador/Entrenador.java
@@ -1,30 +1,46 @@
 package simulador.entrenador;
 
 import java.util.List;
-import java.util.Scanner;
 
-/**
- *
- * @author dfpar
- * @author
- * @author
-// */
 public class Entrenador {
 
     private String nombre;
-    private List<String> pokemones;
+    private List<Pokemon> pokemones;
 
     //Constructor
-    public Entrenador(String nombre, List<String> pokemones) {
+    public Entrenador(String nombre, List<Pokemon> pokemones) {
         this.nombre = nombre;
         this.pokemones = pokemones;
     }
-    Scanner scan = new Scanner(System.in);
-    
+
     //Obtener nombre del entrenador
-    String nombreentrenador = scan.next();
-    public String getnombre() {
-        return nombre = nombreentrenador;
+    public String getNombre() {
+        return nombre;
+    }
+
+    //Agregar un pokemon a la lista
+    //void ya que este metodo no devuelve ningun valor
+    public void agregarPokemonLista(Pokemon pokemon) {
+        pokemones.add(pokemon);
+        //System.out.println("El pokemon "+pokemon.getnombre()+" ha sido añadido al grupo de: "+nombre);
+    }
+
+    //Metodo entrenar al pokemon
+    public void entrenarPokemon(Pokemon pokemon) {
+        //if para verificar que l pokemon este en la lista de pokemones del entrenador
+        if (pokemones.contains(pokemon)) {
+            //hacer condicion en clase pokemon en la que mejora las estadisticas del pokemon
+            //System.out.println("El pokemon: "+pokemon.getNombre+" fue entrenado");
+        } else {
+            System.out.println("El pokemon no se encuentra en el equipo de: " + nombre);
+        }
+    }
+
+    public void mostrarPokemones() {
+        System.out.println("Pokémones de " + nombre + ":");
+        for (Pokemon pokemon : pokemones) {
+            System.out.println(pokemon);
+        }
     }
 
 }

0ddd3dd602df6c16d6d0498f2e3da9a5f94bcab1
Ksrenn <karen170bayona@hotmail.com>
2024-11-13 19:33:22 -0500

    Gestion de entrenadores
    A la clase principal se agrega el case1 que es la gestión de entrenadores

--- src/main/java/simulador/Principal.java
+++ src/main/java/simulador/Principal.java
@@ -1,42 +1,37 @@
 package simulador;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import simulador.entrenador.Entrenador;
 
-/**
- *
- * @author dfpar
- * @author Ksrenn
- * @author
- */
 public class Principal {
 
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        System.out.println("\n--- Menú ---");
-        System.out.println("1. Agregar Pokémon");
-        System.out.println("2. Mostrar lista de Pokémon");
-        System.out.println("3. Salir");
-        System.out.print("Seleccione una opción: ");
-        
-        int opcion= scan.nextInt();
+        int opcion = scan.nextInt();
+        do {
+            System.out.println("--- Menú ---");
+            System.out.println("1. Gestionar Entrenadores");
+            System.out.println("2. Gestionar pokémones");
+            System.out.println("3. Iniciar Batalla");
+            System.out.println("4. Salir");
+            System.out.print("Seleccione una opción: ");
 
-        switch(opcion) {
-        case 1:
-        break;
-        case 2:
-        break;
-        case 3:
-        break;
-        case 4:
-        System.out.println("Ingrese el nombre del entrenador");
-        String nombreentrenador=scan.nextLine();
-        Entrenador nombre = new Entrenador (nombre);
-        
-        break;
-        
-        
-    }
-        
+            scan.nextLine();
+            switch (opcion) {
+                case 1:
+                    gestionarEntrenadores.gestionarEntrenadores(scan);
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    System.out.println("Saliendo del Juego...");
+
+            }
+        } while (opcion != 4);
     }
 }
